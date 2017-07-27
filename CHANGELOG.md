ba18b4e349a3bcd5a5b0dcd04b8c48fb7def5f90
Andy Gutsche <andy.gutsche@aperto.com>
2017-07-27 14:48:03 +0200

    Add properties vc and mod to info

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+### v3.0.1
+- Add properties vc and mod to info
+
 ### v3.0.0
 - Initial version
--- js/toggler.js
+++ js/toggler.js
@@ -2,7 +2,7 @@
  * Represents a simple toggler with global event binding.
  *
  * @module Toggler
- * @version v3.0.0
+ * @version v3.0.1
  *
  * @author Andy Gutsche
  */
@@ -51,7 +51,9 @@ class Toggler extends VeamsComponent {
 	 */
 	static get info() {
 		return {
-			version: '3.0.0'
+			version: '3.0.1',
+			vc: true,
+			mod: false // set to true if source was modified in project
 		};
 	}
 
